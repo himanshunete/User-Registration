e300677ce7f12004f08510c8aad8ecd0d38823f7
himanshunete <himanshuneteh@gmail.com>
2020-12-15 16:27:03 +0530

    UC-2: User need to enter a Valid Last Name

---
+++ EnterAValidLastNameUC2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash/ -x
+
+Pattern_For_Valid_LastName="^[A-Z]{1}[a-z]{2,}$"
+echo Enter Last Name
+read LastName
+if [[ $LastName =~ $Pattern_For_Valid_LastName ]]
+then
+echo Pattern is matching
+else
+echo Pattern is not matching
+fi
